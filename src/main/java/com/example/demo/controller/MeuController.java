fc00216a8eeb2ed297f314ceeafa30d8c59d1d80
ramoneees <ramonp.rios@gmail.com>
2023-03-01 17:26:36 +0000

    Adicionando metodo post

--- src/main/java/com/example/demo/controller/MeuController.java
+++ src/main/java/com/example/demo/controller/MeuController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,11 @@ public class MeuController {
     @GetMapping("/hello")
     public String helloWold() {
         return "Hello world!";
+    }
+
+    @PostMapping
+    public String meumetodo(){
+        return "";
     }
 
 }
